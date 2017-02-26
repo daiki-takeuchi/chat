27636fa9d0f3d9e60596eaf974cd52c08ab67e09
daiki-takeuchi <daiki.takeuchi.0327@gmail.com>
2017-02-26 11:47:53 +0900

    単体テストロジックを追加

---
+++ tests/controllers/post_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from nose.tools import ok_
+
+from application import app
+
+
+class PostTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    # ログインしていない場合は、ログイン画面に移動する。
+    def test_root_status_code(self):
+        result = self.app.get('/')
+
+        self.assertEqual(result.status_code, 302)
+        ok_('/login' in result.headers['Location'])
