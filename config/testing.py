a3844f70e99aace2ed20f6527f5ba14f3f25b05b
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2017-03-02 15:22:50 +0900

    testingのconfigを修正

--- config/testing.py
+++ config/testing.py
@@ -1,13 +1,15 @@
+import os
+
 DEBUG = False
 TESTING = True
 WTF_CSRF_ENABLED = False
 
 # CloudSQL & SQLAlchemy configuration
-HOST = 'localhost'
-PORT = '3306'
-USER = 'chat_user_test'
-PASSWORD = 'chat_user_test'
-DATABASE = 'chat_db_test'
+HOST = os.getenv('MYSQL_PORT_3306_TCP_ADDR', 'localhost')
+PORT = os.getenv('MYSQL_PORT_3306_TCP_PORT', '3306')
+USER = os.getenv('MYSQL_ENV_MYSQL_USER', 'chat_user_test')
+PASSWORD = os.getenv('MYSQL_ENV_MYSQL_PASSWORD', 'chat_user_test')
+DATABASE = os.getenv('MYSQL_ENV_MYSQL_DATABASE', 'chat_db_test')
 SQLALCHEMY_DATABASE_URI = (
     'mysql://{user}:{password}@{host}:{port}/{database}').format(
         user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)
