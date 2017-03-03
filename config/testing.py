0b8436aa95e935added1cc017ac9bcca710bd82f
daiki-takeuchi <daiki.takeuchi.0327@gmail.com>
2017-03-03 10:06:31 +0900

    一部閑居変数をやめたので修正

--- config/testing.py
+++ config/testing.py
@@ -7,9 +7,9 @@ WTF_CSRF_ENABLED = False
 # CloudSQL & SQLAlchemy configuration
 HOST = os.getenv('MYSQL_PORT_3306_TCP_ADDR', 'localhost')
 PORT = os.getenv('MYSQL_PORT_3306_TCP_PORT', '3306')
-USER = os.getenv('MYSQL_ENV_MYSQL_USER', 'chat_user_test')
-PASSWORD = os.getenv('MYSQL_ENV_MYSQL_PASSWORD', 'chat_user_test')
-DATABASE = os.getenv('MYSQL_ENV_MYSQL_DATABASE', 'chat_db_test')
+USER = 'chat_user_test'
+PASSWORD = 'chat_user_test'
+DATABASE = 'chat_db_test'
 SQLALCHEMY_DATABASE_URI = (
     'mysql://{user}:{password}@{host}:{port}/{database}').format(
         user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)
