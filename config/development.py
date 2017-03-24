14008522c03af4a88cb9c354dda7b1d6830768fd
daiki-takeuchi <daiki.takeuchi.0327@gmail.com>
2017-03-24 09:16:47 +0900

    connectionエラーが出るため修正

--- config/development.py
+++ config/development.py
@@ -8,9 +8,11 @@ USER = 'bc490c93bfba0a'
 PASSWORD = 'db4577d5'
 DATABASE = 'heroku_2fe043177fc40e6'
 SQLALCHEMY_DATABASE_URI = (
-    'mysql://{user}:{password}@{host}:{port}/{database}?autoReconnect=true').format(
+    'mysql://{user}:{password}@{host}:{port}/{database}').format(
         user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_POOL_RECYCLE = 499
+SQLALCHEMY_POOL_TIMEOUT = 20
 
 # The secret key is used by Flask to encrypt session cookies.
 SECRET_KEY = 'development key'
