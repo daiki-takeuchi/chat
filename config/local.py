945199c7127fc176d04b3115f2755fbaf8d7c24d
daiki-takeuchi <daiki.takeuchi.0327@gmail.com>
2024-06-23 14:29:04 +0900

    Dockerで起動できるように修正

--- config/local.py
+++ config/local.py
@@ -1,12 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+# .envファイルをロードします。
+# 同じ環境変数名が既に設定されている場合は、値を上書きする
+load_dotenv(override=True)
+
 DEBUG = True
 TESTING = True
 
 # CloudSQL & SQLAlchemy configuration
-HOST = 'localhost'
+HOST = 'db'
 PORT = '3306'
-USER = 'chat_user'
-PASSWORD = 'chat_user'
-DATABASE = 'chat_db'
+USER = os.getenv("MYSQL_USER")
+PASSWORD = os.getenv("MYSQL_PASSWORD")
+DATABASE = os.getenv("MYSQL_DATABASE")
 SQLALCHEMY_DATABASE_URI = (
     'mysql://{user}:{password}@{host}:{port}/{database}').format(
         user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)
